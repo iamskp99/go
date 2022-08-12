62205a1660fa814f5c1bc5d88ce5a49cc43178d7
iamskp99 <iamskp99@gmail.com>
2022-08-12 19:09:12 +0530

    add files

--- fasthttpDemo/main.go
+++ fasthttpDemo/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
-
+	"github.com/keploy/go-sdk/integrations/kfasthttp"
+	"github.com/keploy/go-sdk/keploy"
 	"github.com/valyala/fasthttp"
 )
 
-func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-	typeOfRequest := string(ctx.Method())
-	if typeOfRequest == "GET" {
-		fmt.Fprintln(ctx, "Welcome Its a GET Request!")
-	}
-
-	if typeOfRequest == "POST" {
-		fmt.Fprintln(ctx, "Welcome Its a POST Request!")
-	}
-}
-
 func main() {
-	fasthttp.ListenAndServe(":8080", fastHTTPHandler)
+	k := keploy.New(keploy.Config{
+		App: keploy.AppConfig{
+			Name: "shashankFasthttpDEMO",
+			Port: "8080",
+		},
+		Server: keploy.ServerConfig{
+			URL: "http://localhost:8081/api",
+		},
+	})
+	mw := kfasthttp.FastHttpMiddlware(k)
+	m := func(ctx *fasthttp.RequestCtx) {
+		typeOfRequest := string(ctx.Method())
+		if typeOfRequest == "GET" {
+			fmt.Fprintln(ctx, "Welcome, Its a GET Request!")
+		}
+
+		if typeOfRequest == "POST" {
+			fmt.Fprintln(ctx, "Welcome, Its a POST Request!")
+		}
+	}
+	fasthttp.ListenAndServe(":8080", mw(m))
 }
